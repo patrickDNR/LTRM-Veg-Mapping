68469bb420ba8615c9ab351178377b5f64e0cb9f
Patrick Kelly <patrick.kelly@wisconsin.gov>
2026-08-21 16:13:27 -0500

    Addedpercent frequency data

--- Scripts/veg_dataPrep.R
+++ Scripts/veg_dataPrep.R
@@ -36,5 +36,74 @@ colnames(latlon_df) <- c('lng', 'lat')
 #combine with water quality data
 veg <- cbind(veg, latlon_df)
 
+#get percent frequency for each year by barcode
+veg.freq <- read.csv('Data/ltrm_vegsrs_data.csv')
+
 #Save data...
 write.csv(veg, 'Data/Pool8_AqVeg_SRS.csv')
+
+#make a column of rowSums...just need to see anything greater than 0 counts as a 1...
+#start with unique barcodes
+codes <- unique(veg.freq$BARCODE)
+
+#now get all represented species
+species <- unique(veg.freq$SPPCD)
+
+#make a column of presence/absence
+present <- c()
+for(i in 1:nrow(veg.freq)){
+  if(sum(veg.freq[i,65:70])){
+    present[i] = 1
+  }
+  else{
+    present[i] = 0
+  }
+}
+veg.freq$present <- present
+
+#convert from long to wide format
+#make easier dataframe
+long.df <- data.frame(date = veg.freq$DATE, 
+                      barcode = veg.freq$BARCODE,
+  species = veg.freq$SPPCD, 
+                      value = veg.freq$present)
+
+#remove duplicate rows
+long.df.dup <- long.df[!duplicated(long.df),]
+
+#Still a random duplicate but not duplicate?
+long.df.dup <- long.df.dup[-66693,]
+
+#convert to wide format
+wide.df <- spread(long.df.dup, key = species, value = value, fill = 0)
+
+#fix dates
+wide.df$date <- format(mdy(wide.df$date), '%Y-%m-%d')
+
+#order by date
+wide.df <- wide.df[order(wide.df$date),]
+
+#add year
+wide.df$year <- format(ymd(wide.df$date), '%Y')
+
+#Now get unique years
+years <- unique(wide.df$year)
+
+pct.freq<- c()
+for(i in 1:length(years)){
+  yeari <- wide.df[wide.df$year == years[i],]
+  
+  #make column sums...
+  freq <- (colSums(yeari[,4:ncol(yeari)-1])/nrow(yeari))*100
+  
+  #rebuild data frame
+  x <- data.frame(year = rep(years[i], length(freq)), species = names(freq), 
+                  pct.freq = round(freq, 2))
+  pct.freq <- rbind(pct.freq, x)
+}
+
+#add species names
+pct.freq <- left_join(pct.freq, names, by = c('species' = 'Species.Code'))
+
+#save the file
+write.csv(pct.freq, 'Data/veg_pctFreq_bySpecies.csv')
